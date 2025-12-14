294e85dc3860aefc6230c14a9997aafb92066e5e
Althea <althea.larrauri@gmail.com>
2025-12-14 17:20:48 +0100

    feat: programar ejercicio 1

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class InputSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float playerSpeed = 5f;
+
+    void Ejercicio1()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        float offsetY;
+
+        if (Input.GetButton("Fire3"))
+        {
+            playerSpeed += 1f * Time.deltaTime;
+            float amplitude = 0.05f;
+            float frequency = 4f;
+
+            offsetY = transform.position.y + Mathf.Sin(Time.time * frequency) * amplitude;
+
+        }
+        else
+        {
+            playerSpeed = 5f;
+            offsetY = 0.5f;
+        }
+
+        Vector3 movement = new Vector3(x, 0f, z) * playerSpeed * Time.deltaTime;
+        transform.position += movement;
+
+        transform.position = new Vector3(
+        transform.position.x,
+        offsetY,
+        transform.position.z);
+    }
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ejercicio1();
     }
 }
