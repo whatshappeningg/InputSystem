5a58aac78e18b258ee501cddcfc125595da72f10
Althea <althea.larrauri@gmail.com>
2025-12-14 19:27:07 +0100

    feat: programar ejercicio 2

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -1,9 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class InputSystem : MonoBehaviour
 {
     public float playerSpeed = 5f;
+    public Transform cameraTransform;
 
     void Ejercicio1()
     {
@@ -36,6 +38,31 @@ public class InputSystem : MonoBehaviour
         transform.position.z);
     }
 
+    void Ejercicio2()
+    {
+        float zoomSpeed = 4;
+        float rotationSpeed = 25;
+        float maxDistance = 6f;
+        float minDistance = 2f;
+        float distance;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            cameraTransform.LookAt(transform.position);
+            cameraTransform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed * -1);
+            cameraTransform.Translate(cameraTransform.forward * Input.GetAxis("Vertical") * Time.deltaTime * zoomSpeed);
+
+            distance = Math.Clamp(Vector3.Distance(transform.position, cameraTransform.position), minDistance, maxDistance);
+            Vector3 dir = (cameraTransform.position - transform.position).normalized;
+            cameraTransform.position = transform.position + dir * distance;
+        }
+        else
+        {
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
+            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
+        }
+
+    }
 
     void Start()
     {
@@ -45,6 +72,6 @@ public class InputSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ejercicio1();
+        Ejercicio2();
     }
 }
